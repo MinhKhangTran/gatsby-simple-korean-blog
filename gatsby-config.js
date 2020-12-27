df5435112b227103958c36c1022df47d862e08d5
Minh Khang <minhkhang.tran@gmx.de>
2020-12-27 13:02:04 +0100

    comment stuff for init build

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,20 +5,20 @@ module.exports = {
   plugins: [
     "gatsby-plugin-netlify-cms",
     `@chakra-ui/gatsby-plugin`,
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "static/img",
-      },
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "blog",
-        path: "src/blog",
-      },
-    },
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "images",
+    //     path: "static/img",
+    //   },
+    // },
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "blog",
+    //     path: "src/blog",
+    //   },
+    // },
     {
       resolve: "gatsby-transformer-remark",
       options: {
