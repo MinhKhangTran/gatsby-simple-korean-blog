b2e87398ca9b14f5b24598aba5396a7f7535a5de
Minh Khang <minhkhang.tran@gmx.de>
2020-12-27 12:07:00 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: "simple-gatsby-blog",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-offline",
+  ],
+};
