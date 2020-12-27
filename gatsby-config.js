fea1de0a03d7c4eede956a90f351cb60ee71633d
Minh Khang <minhkhang.tran@gmx.de>
2020-12-27 12:43:32 +0100

    init und config yml init

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,27 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-netlify-cms",
+    `@chakra-ui/gatsby-plugin`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "static/img",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blog",
+        path: "src/blog",
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: ["gatsby-remark-relative-images", "gatsby-remark-images"],
+      },
+    },
     "gatsby-plugin-styled-components",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-offline",
